f827ab3f0ced6b90349716e6a10bb8d4b13603f4
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-10-14 16:54:42 +0100

    Parse reference

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,23 +1,31 @@
-use crate::evaluator::Expr::*;
-
 use nom::IResult;
-use nom::character::complete::{char, digit1, space0};
+use nom::character::complete::{alpha1, digit1, space0};
 use nom::combinator::map;
-use nom::sequence::{delimited, tuple};
+use nom::sequence::{delimited, pair};
 
 type Pos = (char, i32);
 
 enum Expr {
-  Reference(Pos),
   Number(i32),
+  Reference(Pos),
   Binary(Box<Expr>, char, Box<Expr>)
 }
 
-fn parse_num(n: &str) -> Expr {
-    let num = n.parse::<i32>().unwrap();
-    Number(num)
+fn parse_num(number: &str) -> Expr {
+    let num = number.parse::<i32>().unwrap();
+    Expr::Number(num)
 }
 
 fn parse_number(input: &str) -> IResult<&str, Expr> {
     map(delimited(space0, digit1, space0), parse_num)(input)
+}
+
+fn parse_ref((col, row): (&str, &str)) -> Expr {
+    let c = col.chars().nth(0).unwrap();
+    let r = row.parse::<i32>().unwrap();
+    Expr::Reference((c, r))
+}
+
+fn parse_reference(input: &str) -> IResult<&str, Expr> {
+    map(pair(alpha1, digit1), parse_ref)(input)
 }
