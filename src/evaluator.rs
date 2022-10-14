b1a5cb8560e06f214a2dc16c9d666e31eeeb7efd
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-10-14 17:34:28 +0100

    Parse expression

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -31,18 +31,17 @@ fn parse_reference(input: &str) -> IResult<&str, Expr> {
     map(pair(alpha1, digit1), create_reference)(input)
 }
 
-// fn parse_bracket(input: &str) -> IResult<&str, Expr> {
-//     delimited(char('('), delimited(space0, parse_expr, space0), char(')'))(input)
-// }
+fn parse_bracket(input: &str) -> IResult<&str, Expr> {
+    delimited(char('('), delimited(space0, parse_expr, space0), char(')'))(input)
+}
 
-// fn parse_term(input: &str) -> IResult<&str, Expr> {
-//     alt(alt(parse_number, parse_reference), parse_bracket)(input)
-// }
+fn parse_term(input: &str) -> IResult<&str, Expr> {
+    alt((alt((parse_number, parse_reference)), parse_bracket))(input)
+}
 
-// fn create_binary((expr1, operator, expr2): (&Expr, &str, &Expr)) -> Expr {
-//     let op = operator.chars().nth(0).unwrap();
-//     Expr::Binary(Box::new(expr1), op, Box::new(expr2))
-// }
+fn create_binary(((expr1, operator), expr2): ((Expr, char), Expr)) -> Expr {
+    Expr::Binary(Box::new(expr1), operator, Box::new(expr2))
+}
 
 fn parse_operator(input: &str) -> IResult<&str, char> {
     let add_subtract = alt((char('+'), char('-')));
@@ -50,10 +49,10 @@ fn parse_operator(input: &str) -> IResult<&str, char> {
     alt((add_subtract, mult_divide))(input)
 }
 
-// fn parse_binary(input: &str) -> IResult<&str, Expr> {
-//     map(tuple(parse_term, delimited(space0, parse_operator, space0), parse_term), create_binary)(input)
-// }
+fn parse_binary(input: &str) -> IResult<&str, Expr> {
+    map(pair(pair(parse_term, delimited(space0, parse_operator, space0)), parse_term), create_binary)(input)
+}
 
-// fn parse_expr(input: &str) -> IResult<&str, Expr> {
-//     alt(parse_binary, parse_term)(input)
-// }
+fn parse_expr(input: &str) -> IResult<&str, Expr> {
+    alt((parse_binary, parse_term))(input)
+}
