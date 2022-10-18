64c90daf2826f113f730946991fbe5b8eaa7b1b0
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-10-18 22:54:59 +0100

    Introduce custom errors

--- src/parser.rs
+++ src/parser.rs
@@ -5,14 +5,37 @@ use nom::combinator::map;
 use nom::sequence::{delimited, pair, preceded};
 use nom::branch::alt;
 use nom::IResult;
+use nom::Err::Error;
+use nom::error::ParseError;
+use nom::error::ErrorKind;
 
-fn create_number(number: &str) -> Expr {
-    let num = number.parse::<i32>().unwrap();
-    Expr::Number(num)
+#[derive(PartialEq)]
+enum CustomError<I> {
+  Conversion,
+  Nom(I, ErrorKind),
 }
 
-fn parse_number(input: &str) -> IResult<&str, Expr> {
-    map(delimited(space0, digit1, space0), create_number)(input)
+impl<I> ParseError<I> for CustomError<I> {
+  fn from_error_kind(input: I, kind: ErrorKind) -> Self {
+    CustomError::Nom(input, kind)
+  }
+
+  fn append(_: I, _: ErrorKind, other: Self) -> Self {
+    other
+  }
+}
+
+type CResult<I, T> = IResult<I, T, CustomError<I>>;
+
+fn create_number(number: &str) -> CResult<&str, Expr> {
+    match number.parse::<i32>() {
+        Ok(num) => Ok((number, Expr::Number(num))),
+        Err(_) => Err(Error(CustomError::Conversion))
+    }
+}
+
+fn parse_number(input: &str) -> CResult<&str, Expr> {
+    delimited(space0, digit1, space0)(input).and_then(|(_, o)|create_number(o))
 }
 
 fn create_reference((col, row): (&str, &str)) -> Expr {
@@ -21,15 +44,15 @@ fn create_reference((col, row): (&str, &str)) -> Expr {
     Expr::Reference((c, r))
 }
 
-fn parse_reference(input: &str) -> IResult<&str, Expr> {
+fn parse_reference(input: &str) -> CResult<&str, Expr> {
     map(pair(alpha1, digit1), create_reference)(input)
 }
 
-fn parse_bracket(input: &str) -> IResult<&str, Expr> {
+fn parse_bracket(input: &str) -> CResult<&str, Expr> {
     delimited(char('('), delimited(space0, parse_expr, space0), char(')'))(input)
 }
 
-fn parse_term(input: &str) -> IResult<&str, Expr> {
+fn parse_term(input: &str) -> CResult<&str, Expr> {
     alt((parse_number, parse_reference, parse_bracket))(input)
 }
 
@@ -37,25 +60,25 @@ fn create_binary(((expr1, operator), expr2): ((Expr, char), Expr)) -> Expr {
     Expr::Binary(Box::new(expr1), operator, Box::new(expr2))
 }
 
-fn parse_operator(input: &str) -> IResult<&str, char> {
+fn parse_operator(input: &str) -> CResult<&str, char> {
     alt((char('+'), char('-'), char('*'), char('/')))(input)
 }
 
-fn parse_binary(input: &str) -> IResult<&str, Expr> {
+fn parse_binary(input: &str) -> CResult<&str, Expr> {
     map(pair(pair(parse_term, delimited(space0, parse_operator, space0)), parse_term), create_binary)(input)
 }
 
-fn parse_expr(input: &str) -> IResult<&str, Expr> {
+fn parse_expr(input: &str) -> CResult<&str, Expr> {
     alt((parse_binary, parse_term))(input)
 }
 
 // Formula starts with `=` followed by expression
-fn parse_formula(input: &str) -> IResult<&str, Expr> {
+fn parse_formula(input: &str) -> CResult<&str, Expr> {
     preceded(char('='), preceded(space0, parse_expr))(input)
 }
 
 // Equation you can write in a cell is either number or a formula
-fn parse_equation(input: &str) -> IResult<&str, Expr> {
+fn parse_equation(input: &str) -> CResult<&str, Expr> {
     delimited(space0, alt((parse_formula, parse_number)), space0)(input)
 }
 
